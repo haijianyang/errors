aa9c846e539b1e5331e5506470c47c0adebb66e2
haijianyang <weber.yhj@gmail.com>
2022-09-23 14:31:04 +0800

    Add support for code

---
+++ code.go
@@ -0,0 +1,347 @@
+package errors
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"sync"
+)
+
+// Coder defines an interface for an error code detail information.
+type Coder interface {
+	// Code returns the code of the coder
+	Code() string
+
+	// HTTP status that should be used for the associated error code.
+	StatusCode() int
+
+	// External (user) facing error message.
+	Message() string
+
+	// Params returns the params of message.
+	Params() map[string]interface{}
+
+	// FullMessage returns the message with params.
+	FullMessage() string
+
+	// Reference returns the detail documents for user.
+	Reference() string
+}
+
+// codes contains a map of error codes to metadata.
+var codes = map[string]Coder{}
+var codeMux = &sync.Mutex{}
+
+// Register register a user define error code.
+// It will overrid the exist code.
+func Register(coder Coder) {
+	codeMux.Lock()
+	defer codeMux.Unlock()
+
+	codes[coder.Code()] = coder
+}
+
+// MustRegister register a user define error code.
+// It will panic when the same Code already exist.
+func MustRegister(coder Coder) {
+	codeMux.Lock()
+	defer codeMux.Unlock()
+
+	if _, ok := codes[coder.Code()]; ok {
+		panic(fmt.Sprintf("code: %s already exist", coder.Code()))
+	}
+
+	codes[coder.Code()] = coder
+}
+
+// GetCoder return the coder by code.
+func GetCoder(code string) Coder {
+	if coder, ok := codes[code]; ok {
+		return coder
+	}
+
+	return nil
+}
+
+// ParseCoder parse any error into *withCode.
+// nil error will return nil direct.
+// None withCode error will be parsed as nil.
+func ParseCoder(err error) Coder {
+	if err == nil {
+		return nil
+	}
+
+	if wc, ok := err.(*withCode); ok {
+		if coder, ok := codes[wc.code]; ok {
+			return coder
+		}
+	}
+
+	return nil
+}
+
+// IsCode reports whether the error's code is the given code.
+func IsCode(err error, code string) bool {
+	if coder, ok := err.(*withCode); ok {
+		if coder.code == code {
+			return true
+		}
+	}
+
+	return false
+}
+
+// HasCode reports whether any error in err's chain contains the given error code.
+func HasCode(err error, code string) bool {
+	if coder, ok := err.(*withCode); ok {
+		if coder.code == code {
+			return true
+		}
+
+		if coder.cause != nil {
+			return HasCode(coder.cause, code)
+		}
+
+		return false
+	}
+
+	return false
+}
+
+type withCode struct {
+	code    string
+	message string
+	params  map[string]interface{}
+	cause   error
+	*stack
+}
+
+type fullMessage struct {
+	Params  map[string]interface{} `json:"params"`
+	Message string                 `json:"message"`
+}
+
+func (w *withCode) Code() string { return w.code }
+
+func (w *withCode) Message() string { return w.message }
+
+func (w *withCode) Params() map[string]interface{} { return w.params }
+
+func (w *withCode) FullMessage() string {
+	fullMsg := fullMessage{Message: w.message, Params: w.params}
+	if fullMsg.Params == nil {
+		fullMsg.Params = map[string]interface{}{}
+	}
+
+	message, err := json.Marshal(&fullMsg)
+	if err != nil {
+		return "{\"params\":{},\"message\":\"\"}"
+	}
+
+	return string(message)
+}
+
+func (w *withCode) Cause() error { return w.cause }
+
+func (w *withCode) Error() string {
+	errString := w.code + " - " + w.message
+	if w.cause != nil {
+		errString += ": " + w.cause.Error()
+	}
+
+	return errString
+}
+
+// Unwrap provides compatibility for Go 1.13 error chains.
+func (w *withCode) Unwrap() error { return w.cause }
+
+func (w *withCode) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			if w.Cause() != nil {
+				fmt.Fprintf(s, "%+v\n", w.Cause())
+			}
+
+			io.WriteString(s, w.code+" - "+w.message)
+			w.stack.Format(s, verb)
+			return
+		}
+		fallthrough
+	case 's', 'q':
+		io.WriteString(s, w.Error())
+	}
+}
+
+// Code returns the underlying code of the error, if possible.
+// An error value has a cause if it implements the following
+// interface:
+//
+//     type coder interface {
+//            Code() error
+//     }
+//
+// If the error does not implement Code or the error is nil,
+// the empty string will be returned.
+func Code(err error) string {
+	code := ""
+	if err == nil {
+		return code
+	}
+
+	type coder interface {
+		Code() string
+	}
+
+	cd, ok := err.(coder)
+	if ok {
+		code = cd.Code()
+	}
+
+	return code
+}
+
+// Message returns the underlying message of the error, if possible.
+// An error value has a cause if it implements the following
+// interface:
+//
+//     type messager interface {
+//            Message() error
+//     }
+//
+// If the error does not implement Message or the error is nil,
+// the empty string will be returned.
+func Message(err error) string {
+	msg := ""
+	if err == nil {
+		return msg
+	}
+
+	type messager interface {
+		Message() string
+	}
+
+	msger, ok := err.(messager)
+	if ok {
+		msg = msger.Message()
+	}
+
+	return msg
+}
+
+// FullMessage returns the underlying full message of the error, if possible.
+// An error value has a cause if it implements the following
+// interface:
+//
+//     type fullmessager interface {
+//            FullMessage() error
+//     }
+//
+// If the error does not implement FullMessage or the error is nil,
+// the empty string will be returned.
+func FullMessage(err error) string {
+	fullMsg := ""
+	if err == nil {
+		return fullMsg
+	}
+
+	type fullmessager interface {
+		FullMessage() string
+	}
+
+	fullMsger, ok := err.(fullmessager)
+	if ok {
+		fullMsg = fullMsger.FullMessage()
+	}
+
+	return fullMsg
+}
+
+// Params returns the underlying params of the error, if possible.
+// An error value has a cause if it implements the following
+// interface:
+//
+//     type parameter interface {
+//            Params() error
+//     }
+//
+// If the error does not implement parameter or the error is nil,
+// the nil will be returned.
+func Params(err error) map[string]interface{} {
+	if err == nil {
+		return nil
+	}
+
+	type parameter interface {
+		Params() map[string]interface{}
+	}
+
+	paramer, ok := err.(parameter)
+	if ok {
+		return paramer.Params()
+	}
+
+	return nil
+}
+
+// NewCode returns an error with the supplied code and message.
+// NewCode also records the stack trace at the point it was called.
+func NewCode(code string, msgs ...string) error {
+	return &withCode{
+		code:    code,
+		message: message(code, msgs),
+		stack:   callers(),
+	}
+}
+
+func NewCodeWithParams(code string, params map[string]interface{}, msgs ...string) error {
+	return &withCode{
+		code:    code,
+		message: message(code, msgs),
+		params:  params,
+		stack:   callers(),
+	}
+}
+
+// WrapCode returns an error annotating err with a stack trace
+// at the point WrapCode is called, and the supplied code and message.
+// If err is nil, WrapCode returns nil.
+func WrapCode(err error, code string, msgs ...string) error {
+	if err == nil {
+		return nil
+	}
+
+	return &withCode{
+		code:    code,
+		message: message(code, msgs),
+		cause:   err,
+		stack:   callers(),
+	}
+}
+
+func WrapCodeWithParams(err error, code string, params map[string]interface{}, msgs ...string) error {
+	if err == nil {
+		return nil
+	}
+
+	return &withCode{
+		code:    code,
+		message: message(code, msgs),
+		params:  params,
+		cause:   err,
+		stack:   callers(),
+	}
+}
+
+func message(code string, msgs []string) string {
+	message := ""
+	if len(msgs) == 0 {
+		if coder, ok := codes[code]; ok {
+			message = coder.Message()
+		}
+	} else {
+		message = msgs[0]
+	}
+
+	return message
+}
--- example_test.go
+++ example_test.go
@@ -1,20 +1,18 @@
-package errors_test
+package errors
 
 import (
 	"fmt"
-
-	"github.com/pkg/errors"
 )
 
 func ExampleNew() {
-	err := errors.New("whoops")
+	err := New("whoops")
 	fmt.Println(err)
 
 	// Output: whoops
 }
 
 func ExampleNew_printf() {
-	err := errors.New("whoops")
+	err := New("whoops")
 	fmt.Printf("%+v", err)
 
 	// Example output:
@@ -36,24 +34,24 @@ func ExampleNew_printf() {
 }
 
 func ExampleWithMessage() {
-	cause := errors.New("whoops")
-	err := errors.WithMessage(cause, "oh noes")
+	cause := New("whoops")
+	err := WithMessage(cause, "oh noes")
 	fmt.Println(err)
 
 	// Output: oh noes: whoops
 }
 
 func ExampleWithStack() {
-	cause := errors.New("whoops")
-	err := errors.WithStack(cause)
+	cause := New("whoops")
+	err := WithStack(cause)
 	fmt.Println(err)
 
 	// Output: whoops
 }
 
 func ExampleWithStack_printf() {
-	cause := errors.New("whoops")
-	err := errors.WithStack(cause)
+	cause := New("whoops")
+	err := WithStack(cause)
 	fmt.Printf("%+v", err)
 
 	// Example Output:
@@ -89,24 +87,24 @@ func ExampleWithStack_printf() {
 }
 
 func ExampleWrap() {
-	cause := errors.New("whoops")
-	err := errors.Wrap(cause, "oh noes")
+	cause := New("whoops")
+	err := Wrap(cause, "oh noes")
 	fmt.Println(err)
 
 	// Output: oh noes: whoops
 }
 
 func fn() error {
-	e1 := errors.New("error")
-	e2 := errors.Wrap(e1, "inner")
-	e3 := errors.Wrap(e2, "middle")
-	return errors.Wrap(e3, "outer")
+	e1 := New("error")
+	e2 := Wrap(e1, "inner")
+	e3 := Wrap(e2, "middle")
+	return Wrap(e3, "outer")
 }
 
 func ExampleCause() {
 	err := fn()
 	fmt.Println(err)
-	fmt.Println(errors.Cause(err))
+	fmt.Println(Cause(err))
 
 	// Output: outer: middle: inner: error
 	// error
@@ -143,15 +141,15 @@ func ExampleWrap_extended() {
 }
 
 func ExampleWrapf() {
-	cause := errors.New("whoops")
-	err := errors.Wrapf(cause, "oh noes #%d", 2)
+	cause := New("whoops")
+	err := Wrapf(cause, "oh noes #%d", 2)
 	fmt.Println(err)
 
 	// Output: oh noes #2: whoops
 }
 
 func ExampleErrorf_extended() {
-	err := errors.Errorf("whoops: %s", "foo")
+	err := Errorf("whoops: %s", "foo")
 	fmt.Printf("%+v", err)
 
 	// Example output:
@@ -174,10 +172,10 @@ func ExampleErrorf_extended() {
 
 func Example_stackTrace() {
 	type stackTracer interface {
-		StackTrace() errors.StackTrace
+		StackTrace() StackTrace
 	}
 
-	err, ok := errors.Cause(fn()).(stackTracer)
+	err, ok := Cause(fn()).(stackTracer)
 	if !ok {
 		panic("oops, err does not implement stackTracer")
 	}
@@ -193,9 +191,9 @@ func Example_stackTrace() {
 }
 
 func ExampleCause_printf() {
-	err := errors.Wrap(func() error {
+	err := Wrap(func() error {
 		return func() error {
-			return errors.New("hello world")
+			return New("hello world")
 		}()
 	}(), "failed")
 
